16128c8a308632a9cc351dc498f41d4809780acb
gehmlab <gehm@inbox.ru>
2025-01-27 22:52:58 +0300

    Changed store\index.js used configureStore from redux-toolkit Instead of createStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,4 @@
-import { legacy_createStore as createStore, combineReducers, compose, applyMiddleware } from 'redux';
-import { thunk as ReduxThunk } from 'redux-thunk';
+import { configureStore } from '@reduxjs/toolkit';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
@@ -12,32 +11,19 @@ const stringMiddleware = (store) => (next) => (action) => {
   return next(action)
 };
 
-// const enhancer = (createStore) => (...args) => {
-//   const store = createStore(...args);
+const store = configureStore({
+  reducer: {heroes, filters},
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(stringMiddleware),
+  devTools: process.env.NODE_ENV !=='production',
+})
 
-//   const oldDispatch = store.dispatch;
-//   store.dispatch = (action) => {
-//     if (typeof action === 'string') {
-//       return oldDispatch({
-//         type: action
-//       })
-//     }
-//     return oldDispatch(action)
-//   }
-//   return store;
-// }
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore( 
-                    combineReducers({heroes, filters}),
-                    composeEnhancers(applyMiddleware(ReduxThunk, stringMiddleware))
-  
-                    // compose (
-                    //   enhancer,
-                    //   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-                    // )
-                    );
+// const store = createStore( 
+//                     combineReducers({heroes, filters}),
+//                     composeEnhancers(applyMiddleware(ReduxThunk, stringMiddleware))
+//                     );
 
 export default store;
 
