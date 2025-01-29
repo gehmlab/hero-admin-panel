e66600ac330b6be2862bb87751a042406c336f3e
gehmlab <gehm@inbox.ru>
2025-01-29 23:15:55 +0300

    add createEntityAdapter

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -2,8 +2,9 @@ import {useHttp} from '../../hooks/http.hook';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
+import strore from '../../store';
 
-import { filtersChanged, fetchFilters } from './filtersSlice';
+import { filtersChanged, fetchFilters, selectAll} from './filtersSlice';
 import Spinner from '../spinner/Spinner';
 
 // Задача для этого компонента:
@@ -13,11 +14,11 @@ import Spinner from '../spinner/Spinner';
 
 const HeroesFilters = () => {
 
-    const {filters, filtersLoadingStatus, activeFilter} = useSelector(state => state.filters);
+    const {filtersLoadingStatus, activeFilter } = useSelector(state => state.filters);
+    const filters = selectAll(strore.getState());
     const dispatch = useDispatch();
     const {request} = useHttp();
 
-    console.log(filters, "фильтры")
     // Запрос на сервер для получения фильтров и последовательной смены состояния
     useEffect(() => {
         dispatch(fetchFilters());
--- src/components/heroesList/heroesSlice.js
+++ src/components/heroesList/heroesSlice.js
@@ -1,10 +1,17 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk, createEntityAdapter, createSelector } from "@reduxjs/toolkit";
 import { useHttp } from '../../hooks/http.hook';
 
-const initialState = {
-	heroes: [],
-	heroesLoadingStatus: 'idle',
-}
+const heroesAdaptor = createEntityAdapter();
+
+// const initialState = {
+// 	heroes: [],
+// 	heroesLoadingStatus: 'idle',
+// }
+
+const initialState = heroesAdaptor.getInitialState({
+  heroesLoadingStatus: 'idle'
+});
+
 
 export const fetchHeroes = createAsyncThunk(
   'heroes/fetchHeroes',
@@ -19,10 +26,10 @@ const heroesSlice = createSlice({
   initialState,
   reducers: {
     heroCreated: (state, action) => {
-      state.heroes.push(action.payload);
+      heroesAdaptor.addOne(state, action.payload);
     },
     heroDeleted: (state, action) => {
-      state.heroes = state.heroes.filter(item => item.id !== action.payload);
+     heroesAdaptor.removeOne(state, action.payload);
     }
   },
   extraReducers: (builder) => {
@@ -30,7 +37,7 @@ const heroesSlice = createSlice({
       .addCase(fetchHeroes.pending, state => {state.heroesLoadingStatus = 'loading'})
       .addCase(fetchHeroes.fulfilled, (state, action) => {
         state.heroesLoadingStatus = 'idle';
-        state.heroes = action.payload;
+        heroesAdaptor.setAll(state, action.payload)
       })
       .addCase(fetchHeroes.rejected, state => {
         state.heroesLoadingStatus = 'error';
@@ -42,6 +49,22 @@ const heroesSlice = createSlice({
 const {actions, reducer} = heroesSlice;
 
 export default reducer;
+
+const {selectAll} = heroesAdaptor.getSelectors(state => state.heroes);
+
+export const filteredHeroesSelector = createSelector(
+  (state) => state.filters.activeFilter,
+  selectAll,
+  (filter, heroes) => {
+      if (filter === 'all') {
+          return heroes;
+      } else {
+          return heroes.filter(item => item.element === filter)
+      }
+  }
+);
+
+
 export const {
   heroesFetching,
   heroesFetched,
